ebffafcc08eff904c21eda58771bee575dcf669d
Ozik Jarwo <jarwoozik08@gmail.com>
2020-01-03 16:01:10 +0800

    Update cobatescredit.js

--- cobatescredit.js
+++ cobatescredit.js
@@ -4,7 +4,7 @@ function redirect(){
  }, 3000);
 }
 
-if (!$(this).hasClass("test")) {
-  $("html").html("<br/><center><b><h1>REDIRECTED</h1></b><br/><br/><h2>in 3 seconds...</h2></center>");
-  redirect();
-}
+//if (!$(this).hasClass("test")) {
+//  $("html").html("<br/><center><b><h1>REDIRECTED</h1></b><br/><br/><h2>in 3 seconds...</h2></center>");
+//  redirect();
+//}
